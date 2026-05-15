bc124fc3644c61c4f042c70429a74e1d5c04ea61
Rohanz-00 <s4144434@student.rmit.edu.au>
2026-05-15 21:49:48 +1000

    file for clustering comments

---
+++ topic_labels.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#load CSV generated
+df = pd.read_csv("housing_crisis_nlp_ANALYZED.csv")
+
+# the top keywords/examples for each topic to help name them
+# We group by the Topic_Cluster and grab the first 5 comments from each
+topic_summary = df.groupby('Topic_Cluster')['Text'].apply(lambda x: " | ".join(str(i)[:100] + "..." for i in x.head(5))).reset_index()
+
+# Rename columns for clarity
+topic_summary.columns = ['Topic_ID', 'Example_Comments_Preview']
+
+# Save as a reference
+topic_summary.to_csv("topic_dictionary.csv", index=False)
+
+print("Topic Summary Generated")
+print("Open 'topic_dictionary.csv' to see what people are talking about in each cluster.")
